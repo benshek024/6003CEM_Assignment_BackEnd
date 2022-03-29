20bcbb675c6e37396e449fb9c1bec51f00433299
Ben Shek <shek798@gmail.com>
2022-03-29 19:12:48 +0000

    Added workers model

---
+++ models/workers.js
@@ -0,0 +1,16 @@
+const dbMongo = require('../helpers/mongodb')
+
+exports.getAll = async function getAll (page, limit, order){
+  let data = await dbMongo.run_query('workers', {})
+  return data
+}
+
+exports.getByID = async function getByID (id){
+  let data = await dbMongo.run_query('workers', {'staffID': parseInt(id)})
+  return data
+}
+
+exports.add = async function add (document) {
+  let status = await dbMongo.run_insert('workers', document)
+  return status
+}
